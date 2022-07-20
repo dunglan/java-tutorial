0f21d4be139126ffc018e60460ed4be0f7066de9
Lan Dung <dung.lan@gmail.com>
2022-07-20 12:52:23 -0700

    Create print.pattern.java

---
+++ print.pattern.java
@@ -0,0 +1,19 @@
+/******
+
+Create a function that given r and N times will print a run of 1's and 0's of length r.
+
+Examples: 
+
+r=3 n=1
+Output: 000
+
+r=3 n=2
+Output: 000111
+
+r=2 n=3
+Output: 001100
+
+r=1 n=3
+Output 010
+
+*******/
