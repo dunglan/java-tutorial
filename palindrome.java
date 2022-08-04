dbefc1248bd6cb2b00ccb7cfd7525ad2b9d7e199
Lan Dung <dung.lan@gmail.com>
2022-08-04 08:49:55 -0700

    Create palindrome.java

---
+++ palindrome.java
@@ -0,0 +1,12 @@
+/**
+
+Write a method isPalindrome() that given a string S will return true if s is a palindrome
+
+isPalindrome("abba") returns true
+
+isPalindrome("aba") returns true
+
+isPalindrome("abc") returns false
+
+
+**/
